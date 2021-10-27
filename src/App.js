5ecff0982afa69404b6a80ebe768928e0100adf1
Jhose Portillo <jhosmar26.00@gmail.com>
2021-10-26 20:01:35 -0500

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header test">
-        Demo
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header test">
+          <Link to="/home">
+            Logo
+          </Link>
+          <Link to="/contact">Llámanos</Link>
+        </header>
+        <Switch>
+          <Route path="/home">
+            casa dulce casa
+          </Route>
+          <Route path="/contact">
+            llama porfavor
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/home"></Redirect>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
