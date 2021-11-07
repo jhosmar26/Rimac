d59cbfdae23f4410a56d96994e6c5e04e0b4a2bc
Jhose Portillo <jhosmar26.00@gmail.com>
2021-11-07 15:09:27 -0500

    add boy-section

--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -1,4 +1,5 @@
 import "./Plan.scss";
+import rimacBoy from "./../assets/images/rimac-boy.png";
 import { Link } from "react-router-dom";
 
 export const Plan = () => {
@@ -11,6 +12,25 @@ export const Plan = () => {
         <div className="steps__text">Paso 1 de 2</div>
         <div className="steps__progress-bar"></div>
       </div>
+      <section className="boy-section">
+        <div className="boy-section__top">
+          <div className="boy-section__title--web">
+            ¡Hola, <span className="red">Juan!</span>
+          </div>
+          <div className="boy-section__title">Mira las coberturas</div>
+          <div className="boy-section__subtitle">
+            Conoce las coberturas para tu plan
+          </div>
+          <div className="card">
+            <div className="card__text">
+              <div className="placa">Placa: C2U-114</div>
+              <div className="car-details">Wolkswagen 2019 Golf</div>
+            </div>
+            <img className="card__boy" src={rimacBoy} alt="rimac-boy" />
+          </div>
+        </div>
+        <div class="card-data"></div>
+      </section>
     </>
   );
 };
