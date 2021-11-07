ebc6cb07849447b851a88c678b78762d240e65b8
Jhose Portillo <jhosmar26.00@gmail.com>
2021-11-06 19:09:43 -0500

    add custom checkbox in termns

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -38,7 +38,12 @@ export const Home = () => {
           </label>
         </div>
         <div className="termns">
-          <input type="checkbox" className="termns__checkbox"/>
+          <input type="checkbox" className="termns__checkbox" hidden required/>
+          <div className="termns__checkbox-custom">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
           <div className="text">
             Acepto la <a href="" className="link">Política de Protección de Datos Personales</a> y los <a className="link" href="">Términos y Condiciones.</a>
           </div>
